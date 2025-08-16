abbb5cd4cf9bf3c40e80b61360068faa109c93d0
0xferdv <qdsjprs@gmail.com>
2025-08-16 23:04:16 +0800

    增加pigeonhole_sort排序算法

--- crates/sorting/src/lib.rs
+++ crates/sorting/src/lib.rs
@@ -59,6 +59,10 @@ pub use pancake_sort::*;
 pub mod patience_sort;
 pub use patience_sort::*;
 
+pub mod pigeonhole_sort;
+pub use pigeonhole_sort::*;
+
+
 
 #[cfg(test)]
 /// 检查两个切片是否包含相同的元素（不考虑顺序）
